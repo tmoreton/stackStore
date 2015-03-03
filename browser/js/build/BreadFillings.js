07fdfabddb2103c381c3b68bc6a883655b2dc07a
Linda Kung <lindakung417@gmail.com>
2015-03-03 16:36:33 -0500

    added a state called build

---
+++ browser/js/build/BreadFillings.js
@@ -0,0 +1,23 @@
+
+'use strict';
+app.factory('BreadFillings', function () {
+
+    var bread = [
+        'Wheat',
+        'White',
+        'Rye',
+        'Brioche',
+        'Baguette',
+        'Wrap',
+        'Pumpernickel',
+        'Ciabatta',
+        'Multigrain',
+        'Pretzel Roll'
+    ];
+
+    return {
+        bread : bread,
+        
+    };
+
+});
