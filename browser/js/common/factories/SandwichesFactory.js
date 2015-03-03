b2d80370eb746c5b69673f8d0052fba15236e5cc
Linda Kung <lindakung417@gmail.com>
2015-03-03 09:43:11 -0500

    spelling mistake

--- browser/js/common/factories/SandwichesFactory.js
+++ browser/js/common/factories/SandwichesFactory.js
@@ -12,7 +12,7 @@ app.factory('SandwichesFactory', function ($http) {
         },
 
         addNewSandwich: function(sandwich) {
-            return $http.post('/sadwiches', sandwich);
+            return $http.post('/sandwiches', sandwich);
         }
     }
 
