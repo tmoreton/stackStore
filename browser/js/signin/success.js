0726e3ada4fa37c464957d6a0b7ce5f3cb3811d0
Sarah Muenzinger <sarah.muenzinger@gmail.com>
2015-03-05 18:53:20 -0500

    success page improved into a user page with some basic user info

--- browser/js/signin/success.js
+++ browser/js/signin/success.js
@@ -16,7 +16,7 @@ app.config(function ($stateProvider) {
     	},
 	    url: '/success',
 	    controller: 'SuccessCtrl',
-	    template: "<img style='height:250px' src='http://nu.spoonuniversity.com/wp-content/uploads/sites/2/2013/03/waffle.jpg'><h1>Much Success!</h1>"
+	    templateUrl: "js/profile/profile.html"
     });
 });
 
