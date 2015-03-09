36e88261e23bffa996ea0f6e4566a24c433be0e0
Seema <seemaullal@gmail.com>
2015-03-08 23:39:09 -0400

    Added ability to add custom picture of sandwich from database. Updated seed file to demo this.

--- browser/js/addsandwich/addSandwichState.js
+++ browser/js/addsandwich/addSandwichState.js
@@ -12,6 +12,9 @@ app.config(function ($stateProvider) {
 app.controller('AddSandwichCtrl', function ($scope, SandwichesFactory, $kookies, CookieFactory) {
 	SandwichesFactory.getSandwiches().then( function(sandwiches) {
 		$scope.sandwichSelection = sandwiches;
+		angular.forEach($scope.sandwichSelection, function (sandwich) {
+			sandwich.image = sandwich.image? sandwich.image : "http://fc00.deviantart.net/fs70/f/2012/178/c/e/sandwich_icon_by_yamshing-d553fv4.png";
+		});
 	});
 	var storedCookies = CookieFactory.getCookies();
 	if (storedCookies) {
