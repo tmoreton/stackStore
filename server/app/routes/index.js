34e1eea276c2cabe3bf9ff41dc14c2645fd559f3
Seema <seemaullal@gmail.com>
2015-03-09 21:37:45 -0400

    Populate reviews on sandwich route

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -2,7 +2,7 @@
 var router = require('express').Router();
 var Sandwich = require('../../db/models/sandwich.js').Sandwich;
 var User = require('../../db/models/user.js').User;
-var Reviews = require('../../db/models/reviews.js').Reviews
+var Reviews = require('../../db/models/reviews.js').Reviews;
 var Order = require('../../db/models/orders.js').Order;
 var stripe = require("stripe")("sk_test_Cv1UxGFrtA7dBCrUWstCn5sA");
 
@@ -13,7 +13,7 @@ router.get('/reviews', function(req, res){
     if(!req.body.params) {
       Reviews.find({}, function(err, reviews) {
         if(err) {
-          res.send(err)
+          res.send(err);
         }
         res.json(reviews);  
      });
@@ -25,17 +25,17 @@ router.get('/sandwiches/:id', function(req, res) {
     Sandwich.findById(req.params.id, function(err, sandwich){
       if(err) res.send(err);
         res.json(sandwich);
-    })
+    });
 
-})
+});
 
 //get all sandwiches
 router.get('/sandwiches', function(req, res){
-  Sandwich.find({}, function(err, sandwiches) {
-    if(err) res.send(err)
+  Sandwich.find().populate('reviews').exec( function(err, sandwiches) {
+    if(err) res.send(err);
       res.json(sandwiches);
-    })
-})
+  });
+});
 
 
 //post sandwiches
@@ -46,8 +46,8 @@ router.post('/sandwiches/', function(req, res) {
       res.send(err).end();
     }
 		res.json(sandwich);
-	})
-})
+	});
+});
 
 
 router.delete('/sandwiches/:id', function(req, res) {
@@ -57,13 +57,11 @@ router.delete('/sandwiches/:id', function(req, res) {
     if(err) res.send(err);
 
     Sandwich.find(function(err, sandwiches) {
-      if(err) res.send(err)
+      if(err) res.send(err);
         res.json(sandwiches);
-    })
-
-
-  })
-})
+    });
+  });
+});
 
 router.put('/sandwiches/:id', function(req, res) {
   Sandwich.findById(req.params.id, function(err, sandwich) {
@@ -73,46 +71,46 @@ router.put('/sandwiches/:id', function(req, res) {
     sandwich.save(function(err) {
       if(err) res.send(err);
       res.json(sandwich);
-    })
-  })
-})
+    });
+  });
+});
 
 router.post('/signup/', function(req, res){
-  console.log(req.body.firstName)
+  console.log(req.body.firstName);
   User.create({
     firstName: req.body.firstName,
     lastName: req.body.lastName,
     password: req.body.password,
     email: req.body.email
   }).then(function(person){
-    console.log("Got this far!")
+    console.log("Got this far!");
     res.status(200).end();
-  })
+  });
 });
 router.post('/orders', function(req, res){
-  console.log("this is req.body or so we thought", req.body)
+  console.log("this is req.body or so we thought", req.body);
   Order.create({sandwiches:req.body.sandwiches, user:req.body.user}).then(function(order){
     console.log("order has been created");
-    console.log("this should be a user damnit", req.body.user)
+    console.log("this should be a user damnit", req.body.user);
     User.findById(req.body.user, function(err, userDoc){
-      console.log("inside user function")
+      console.log("inside user function");
       if (err){
-        console.log(err)
+        console.log(err);
       }else{
-        console.log("trying to update user")
+        console.log("trying to update user");
         userDoc.orders.push(order);
         userDoc.save(function(){
           res.status(200).end();
         });
       }
     });
-  })
-})
+  });
+});
 
 
 router.post('/charge', function(req, res){
   var stripeToken = req.body.stripeToken;
-    console.log("----------------------------req-------",req.body)
+    console.log("----------------------------req-------",req.body);
   var charge = stripe.charges.create({
     amount: req.body.sandwiches.price, // amount in cents, again
     currency: "usd",
@@ -124,7 +122,7 @@ router.post('/charge', function(req, res){
       console.log("line 113", JSON.stringify(err, null, 2));
     }
     // console.log(charge)
-    res.send("completed payment!")
+    res.send("completed payment!");
   });
 });
 
