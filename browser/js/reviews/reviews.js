cf45aa00580e90455f7ad9207751c9da0ed09090
Seema <seemaullal@gmail.com>
2015-03-10 00:24:19 -0400

    Review page is functional. Can show all reviews or just those for a selected sandwich

--- browser/js/reviews/reviews.js
+++ browser/js/reviews/reviews.js
@@ -12,11 +12,23 @@ app.config(function ($stateProvider) {
 app.controller('ReviewCtrl', function ($scope, $state, SandwichesFactory) {
 
     SandwichesFactory.getSandwiches().then (function(sandwiches) {
+        $scope.allSandwiches = sandwiches;
         $scope.sandwiches = sandwiches;
+
     });
+
+    $scope.sandwichSelected = function() {
+        $scope.sandwiches = [ $scope.selectedSandwich ];
+    };
 
     $scope.getStarNum = function(review) {
         return new Array(review.stars);
+    };
+
+    $scope.showAllReviews = function() {
+
+        $scope.sandwiches = $scope.allSandwiches;
+        $scope.selectedSandwich = "";
     };
 
 });
